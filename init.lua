e7c51e16d6b3d86659acd570512aec48f13f002c
dami <damianroig@gmail.com>
2023-11-04 12:52:05 -0300

    added nvim-surround plugin

--- init.lua
+++ init.lua
@@ -354,6 +354,18 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
 }, {})
 
 -- [[ Setting options ]]
