9bc55b0edfffcfbe99b5bce3219dd2ddd4dd341c
dami <damianroig@gmail.com>
2024-10-26 07:57:24 -0300

    feat: add blinking cursor

--- init.lua
+++ init.lua
@@ -157,6 +157,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Set blinking cursor
+vim.o.guicursor = 'a:blinkon100'
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
