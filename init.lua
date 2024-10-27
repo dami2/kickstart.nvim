9cb5d0368489995b6351ff533444dee5e282c1ea
dami <damianroig@gmail.com>
2024-10-27 08:39:06 -0300

    feat: better quick fix plugin

--- init.lua
+++ init.lua
@@ -1093,7 +1093,14 @@ require('lazy').setup({
     init = function()
       vim.g.camelcasemotion_key = '<leader>'
     end
-  }
+  },
+  -- Better quickfix window in Neovim
+  {
+    "kevinhwang91/nvim-bqf",
+    event = "VeryLazy",
+    opt = {}
+  },
+
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
