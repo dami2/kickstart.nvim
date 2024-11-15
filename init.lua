34b7d673e225404aaadbbbe0a1b2e55056a1ca07
dami <damianroig@gmail.com>
2024-11-15 15:50:41 -0300

    feat: mapping for yank to clipboard

--- init.lua
+++ init.lua
@@ -182,6 +182,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Personal Mappings
+
+-- Mappigs for yank to clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = '[y] Yank to clipboard' })
+vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = '[Y] Yank line to clipboard' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
