c842cbd55a58796b3c3d7f662dd4172e27b99e67
dami <damianroig@gmail.com>
2024-11-15 17:11:34 -0300

    feat: enable recomended plugins

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,13 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      enabled = true,
+      debounce = 500,
+      indent = { char = '┊' },
+    },
+    -- keys = {
+    --   { '<leader>ti', '<CMD>IBLToggle<CR>', desc = '[ti] Toggle ident-blankline' },
+    -- },
   },
 }
