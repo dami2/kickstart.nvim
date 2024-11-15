401b0a868ab7e7a4380813820f47d59a3bfa5fd1
dami <damianroig@gmail.com>
2024-11-15 16:53:51 -0300

    feat: add precognition

--- init.lua
+++ init.lua
@@ -1100,6 +1100,34 @@ require('lazy').setup({
     opts = {},
     event = 'VeryLazy',
   },
+
+  {
+    'tris203/precognition.nvim',
+    event = 'VeryLazy',
+    opts = {
+      startVisible = true,
+      showBlankVirtLine = false,
+      -- highlightColor = { link = "Comment" },
+      -- hints = {
+      --      Caret = { text = "^", prio = 2 },
+      --      Dollar = { text = "$", prio = 1 },
+      --      MatchingPair = { text = "%", prio = 5 },
+      --      Zero = { text = "0", prio = 1 },
+      --      w = { text = "w", prio = 10 },
+      --      b = { text = "b", prio = 9 },
+      --      e = { text = "e", prio = 8 },
+      --      W = { text = "W", prio = 7 },
+      --      B = { text = "B", prio = 6 },
+      --      E = { text = "E", prio = 5 },
+      -- },
+      -- gutterHints = {
+      --     G = { text = "G", prio = 10 },
+      --     gg = { text = "gg", prio = 9 },
+      --     PrevParagraph = { text = "{", prio = 8 },
+      --     NextParagraph = { text = "}", prio = 8 },
+      -- },
+    },
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
