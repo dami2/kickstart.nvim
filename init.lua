9b1e5ca03b2c41db9669155665bb6bc2021eafac
dami <damianroig@gmail.com>
2024-11-15 15:59:08 -0300

    feat: mapping for displaying diagnostics

--- init.lua
+++ init.lua
@@ -164,6 +164,7 @@ vim.o.guicursor = 'a:blinkon100'
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Op[E]n floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
