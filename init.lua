1e123cfbf7940cedc4473e9b31ff45c35cf81a65
dami <damianroig@gmail.com>
2024-11-15 15:33:55 -0300

    feat: add fugitive

--- init.lua
+++ init.lua
@@ -943,6 +943,10 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+
+  -- Personal Plugins
+
+  'tpope/vim-fugitive',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
