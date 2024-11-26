ec5fb5c94f31a448c206e628af0c86ea5ae24955
dami <damianroig@gmail.com>
2024-11-26 13:14:01 -0300

    feat: change background color when switching modes

--- init.lua
+++ init.lua
@@ -257,6 +257,27 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Personal Autocommands
+
+-- Change the background color when switching modes
+vim.api.nvim_create_augroup('InsertModeColors', { clear = true })
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = 'InsertModeColors',
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'Normal', { bg = '#191A21', ctermbg = 'black' })
+  end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  group = 'InsertModeColors',
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'Normal', { bg = '#303030', ctermbg = 'NONE' })
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
